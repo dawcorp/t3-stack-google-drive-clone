0b679f21ecb01c9bf850d1643346feddac4d418f
Davide Voza <davide.voza@mozart.it>
2025-03-25 15:42:00 +0100

    feat: enhance database schema with files and folders tables, and implement dynamic table creation

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,38 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import {
+  int,
+  text,
+  singlestoreTable,
+  index,
+  singlestoreTableCreator,
+  bigint,
+} from "drizzle-orm/singlestore-core"
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+export const createTable = singlestoreTableCreator(
+  (name) => `drive_tutorial_${name}`,
+)
+
+export const files = createTable(
+  "files_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    url: text("url").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+    size: text("size").notNull(),
+  },
+  (t) => [index("parent_index_file").on(t.parent)],
+)
+
+export const folders = createTable(
+  "folders_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (t) => [index("parent_index_folder").on(t.parent)],
+)
