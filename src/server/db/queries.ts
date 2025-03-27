8dc18ee2ffa92a5ae34c563dbc3ab63c619c0add
Davide Voza <davide.voza@mozart.it>
2025-03-27 16:08:52 +0100

    refactor: encapsulate database queries for folders and files within a QUERIES object for better organization

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,3 +1,5 @@
+import "server-only"
+
 import { eq } from "drizzle-orm"
 import { db } from "~/server/db"
 import {
@@ -5,40 +7,35 @@ import {
   folders_table as FolderSchema,
 } from "~/server/db/schema"
 
-export async function getAllParentsForFolder(folderId: number) {
-  const parents = []
-  let currentId: number | null = folderId
-  while (currentId !== null) {
-    const folder = await db
-      .selectDistinct()
-      .from(FolderSchema)
-      .where(eq(FolderSchema.id, currentId))
-    if (!folder[0]) {
-      throw new Error("Parent folder not found")
+export const QUERIES = {
+  getAllParentsForFolder: async function getAllParentsForFolder(
+    folderId: number,
+  ) {
+    const parents = []
+    let currentId: number | null = folderId
+    while (currentId !== null) {
+      const folder = await db
+        .selectDistinct()
+        .from(FolderSchema)
+        .where(eq(FolderSchema.id, currentId))
+      if (!folder[0]) {
+        throw new Error("Parent folder not found")
+      }
+      parents.unshift(folder[0])
+      currentId = folder[0]?.parent
     }
-    parents.unshift(folder[0])
-    currentId = folder[0]?.parent
-  }
-  return parents
+    return parents
+  },
+  getFolders: function getFolders(parsedFolderId: number) {
+    return db
+      .select()
+      .from(FolderSchema)
+      .where(eq(FolderSchema.parent, parsedFolderId))
+  },
+  getFiles: function getFiles(parsedFolderId: number) {
+    return db
+      .select()
+      .from(FileSchema)
+      .where(eq(FileSchema.parent, parsedFolderId))
+  },
 }
-
-export function getFolders(parsedFolderId: number) {
-  return db
-    .select()
-    .from(FolderSchema)
-    .where(eq(FolderSchema.parent, parsedFolderId))
-}
-
-export function getFiles(parsedFolderId: number) {
-  return db
-    .select()
-    .from(FileSchema)
-    .where(eq(FileSchema.parent, parsedFolderId))
-}
-
-// const parentsPromise = getAllParents(parsedFolderId)
-// const [folders, files, parents] = await Promise.all([
-//   foldersPromise,
-//   filesPromise,
-//   parentsPromise,
-// ])
