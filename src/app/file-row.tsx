8b2aaa7b1f4780af450c0de34c2d0618e7c12776
Davide Voza <davide.voza@mozart.it>
2025-03-27 12:57:38 +0100

    refactor: rename database schema types for files and folders to improve clarity and consistency

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,8 +1,8 @@
 import { FileIcon, Folder } from "lucide-react"
 import Link from "next/link"
-import type { files, folders } from "~/server/db/schema"
+import type { files_table, folders_table } from "~/server/db/schema"
 
-export function FileRow(props: { file: typeof files.$inferSelect }) {
+export function FileRow(props: { file: typeof files_table.$inferSelect }) {
   const { file } = props
   return (
     <li
@@ -26,7 +26,9 @@ export function FileRow(props: { file: typeof files.$inferSelect }) {
   )
 }
 
-export function FolderRow(props: { folder: typeof folders.$inferSelect }) {
+export function FolderRow(props: {
+  folder: typeof folders_table.$inferSelect
+}) {
   const { folder } = props
   return (
     <li
