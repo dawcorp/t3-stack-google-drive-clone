98a231dfb94fe9f76ed97a60c2efe50b2e092302
Davide Voza <davide.voza@mozart.it>
2025-03-27 12:51:50 +0100

    feat: implement breadcrumb navigation and link handling for folders in DriveContents and FileRow components

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -6,6 +6,23 @@ import {
   folders as FolderSchema,
 } from "~/server/db/schema"
 
+async function getAllParents(folderId: number) {
+  const parents = []
+  let currentId: number | null = folderId
+  while (currentId !== null) {
+    const folder = await db
+      .selectDistinct()
+      .from(FolderSchema)
+      .where(eq(FolderSchema.id, currentId))
+    if (!folder[0]) {
+      throw new Error("Parent folder not found")
+    }
+    parents.unshift(folder[0])
+    currentId = folder[0]?.parent
+  }
+  return parents
+}
+
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }> // usiamo Promise per evitare che la route venga cacheata
 }) {
@@ -14,14 +31,22 @@ export default async function GoogleDriveClone(props: {
   if (isNaN(parsedFolderId)) {
     return <div>Invalid folder ID</div>
   }
-  const folders = await db
+  const foldersPromise = await db
     .select()
     .from(FolderSchema)
     .where(eq(FolderSchema.parent, parsedFolderId))
-  const files = await db
+
+  const filesPromise = await db
     .select()
     .from(FileSchema)
     .where(eq(FileSchema.parent, parsedFolderId))
 
-  return <DriveContents files={files} folders={folders} />
+  const parentsPromise = getAllParents(parsedFolderId)
+  const [folders, files, parents] = await Promise.all([
+    foldersPromise,
+    filesPromise,
+    parentsPromise,
+  ])
+
+  return <DriveContents files={files} folders={folders} parents={parents} />
 }
